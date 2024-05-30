577093e713220dc9e0b6aa4ce555565090217570
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-05-29 22:35:43 -0300

    feat: configuration of the app file in the project source

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+// src/app.ts
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import userRoutes from './routes/userRoutes';
+import errorHandler from './middleware/errorHandler';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(express.json());
+// TODO: revisar documentacion
+app.use(cors());
+// TODO: revisar documentacion
+app.use(helmet());
+// TODO: revisar documentacion
+app.use(morgan('combined'));
+
+// Routes
+app.use('/api', userRoutes);
+
+// Error handling middleware
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
