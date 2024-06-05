7db70bf0e30be7295ef3332588e29e9a071e7ac3
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-06-05 16:32:38 -0300

    feat: check database connection and node --watch for changes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import morgan from 'morgan';
+
 // import helmet from 'helmet';
 import userRoutes from './routes/userRoutes.ts';
 import errorHandler from './middleware/errorHandler.ts';
+import { testConnection } from './prisma.ts';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,8 +15,6 @@ app.use(express.json());
 app.use(cors());
 // TODO: revisar documentacion
 // app.use(helmet());
-// TODO: revisar documentacion
-app.use(morgan('combined'));
 
 // Routes
 app.use('/api', userRoutes);
@@ -24,5 +23,6 @@ app.use('/api', userRoutes);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
+  testConnection();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
--- src/prisma.ts
+++ src/prisma.ts
@@ -2,4 +2,15 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+export const testConnection = async (): Promise<void> => {
+  try {
+    await prisma.$connect();
+    console.log('Conexión a PostgreSQL exitosa');
+  } catch (error) {
+    console.error('Error al conectar a PostgreSQL:', error);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
 export default prisma;
