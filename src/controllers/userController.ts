c937772d2fff3a40f02fc6f042a24e5e4b733a0a
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-06-05 20:37:28 -0300

    feat: error handling in user creation

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import * as userService from '../services/userService.js';
 import type { CreateUserDto } from '@/models/types/user.d.ts';
+import log from '@/utils/logger.ts';
 
 export const getUsers = async (req: Request, res: Response): Promise<void> => {
   const users = await userService.getAllUsers();
@@ -11,6 +12,19 @@ export const createUser = async (
   req: Request<{}, {}, CreateUserDto>,
   res: Response
 ): Promise<void> => {
-  const user = await userService.createUser(req.body);
-  res.json(user);
+  try {
+    const user = await userService.createUser(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    if (
+      error instanceof Error &&
+      error.message ===
+        'No se puede crear un usuario con el rol de administrador'
+    )
+      res.status(400).json({ error: error.message });
+    else {
+      log('error', 'Error creating user', { error });
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
 };
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,25 +1,39 @@
 import type { User } from '@/models/User/user.ts';
+import { v4 as uuidv4 } from 'uuid';
 import prisma from '../prisma.ts';
 import type { CreateUserDto } from '@/models/types/user.d.ts';
 import { encryptPassword } from '@/utils/encryption.ts';
+import { UserRole } from '@prisma/client';
+import log from '@/utils/logger.ts';
 
 export const getAllUsers = async (): Promise<User[]> => prisma.user.findMany();
 
 export const createUser = async (userData: CreateUserDto): Promise<User> => {
   const encryptedPassword = await encryptPassword(userData.password);
+  const uuid = uuidv4();
 
-  // TODO: no permitir userRole Admin
+  if (userData.role === UserRole.ADMIN)
+    throw new Error('No se puede crear un usuario con el rol de administrador');
 
-  const user = await prisma.user.create({
-    data: {
-      ...userData,
-      password: encryptedPassword,
-      oldPassword: null,
-    },
-  });
-  return user;
+  // TODO: verificar que el email no se encuentre registrado
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        ...userData,
+        id: uuid,
+        password: encryptedPassword,
+        oldPassword: null,
+      },
+    });
+    return user;
+  } catch (error) {
+    log('error', 'Error creating user', { error });
+    throw error;
+  }
 };
 
+// TODO: login
 // export const loginUser = async (userName: string, password: string): Promise<User | null> => {
 //   const user = await prisma.user.findUnique({
 //     where: {
