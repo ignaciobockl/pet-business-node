6e48e3ab7bbc9bdf75e5aed7b1543482af02df7f
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-06-05 17:28:52 -0300

    feat: user model in process

--- src/models/Person/person.ts
+++ src/models/Person/person.ts
@@ -1,5 +1,12 @@
 export interface Person {
   id: string;
+  firstName: string;
+  lastName: string;
+  dni: string;
+  cuilCuit: string;
+  cellPhone?: string;
+  // address
+  userId: string;
   createdAt: Date;
-  updatedAt: Date;
+  updatedAt?: Date | null;
 }
