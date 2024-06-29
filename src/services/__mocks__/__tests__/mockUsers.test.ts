30feb69969adec08aa4d9598e1edc8aa9c75bc96
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-06-29 18:12:31 -0300

    test: generateMockUsers - should generate mock users with expected properties - verification for oldPassword

--- src/services/__mocks__/__tests__/mockUsers.test.ts
+++ src/services/__mocks__/__tests__/mockUsers.test.ts
@@ -1,23 +1,28 @@
 import { UserRole } from '@prisma/client';
 
 import { User } from '../../../models/User/user.ts';
-import { generateMockCreateUser, generateMockUsers } from '../mockUsers.ts';
 import prisma from '../../../prisma.ts';
+import { generateMockCreateUser, generateMockUsers } from '../mockUsers.ts';
 
 describe('generateMockUsers', () => {
   it('should generate mock users with expected properties', async () => {
+    await prisma.user.deleteMany();
+
     const mockUsers: User[] = await generateMockUsers();
 
-    expect(mockUsers).toHaveLength(2);
+    expect(mockUsers).toHaveLength(3);
 
     mockUsers.forEach((user) => {
       expect(user.id).toBeTruthy();
       expect(user.userName).toBeTruthy();
       expect(user.password).toBeTruthy();
-      expect(user.oldPassword).toBeDefined();
       expect(user.mail).toBeTruthy();
       expect(user.createdAt).toBeTruthy();
       expect(user.updatedAt).toBeDefined();
+
+      if (user.oldPassword) {
+        expect(user.oldPassword).toBeTruthy();
+      } else expect(user.oldPassword).toBeDefined();
     });
 
     expect(mockUsers[0].role).toBe(UserRole.USER);
@@ -28,7 +33,6 @@ describe('generateMockUsers', () => {
 describe('generateMockCreateUser', () => {
   it('should generate a mock user with expected properties', async () => {
     const mockUser: User[] = await generateMockCreateUser();
-    console.log('🍔 ~ it ~ mockUser:', mockUser);
 
     expect(mockUser).toHaveLength(1);
 
