202d0eb1423b7ee16e000b7d8a82a026bc870556
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-06-30 12:47:52 -0300

    feat: add missingFields prop (optional) to the createValidationError function

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,21 +1,27 @@
 interface ValidationError extends Error {
   details?: unknown;
+  missingFields?: string[];
 }
 
 /**
- * Creates a validation error with the specified message and optional details.
+ * Creates a validation error with the specified message, optional details, and missing fields.
  * @param {string} message The error message.
  * @param {unknown} [details] Optional details to include in the error.
+ * @param {string[]} [missingFields] Optional missing fields to include in the error.
  * @returns {ValidationError} The validation error.
  */
 const createValidationError = (
   message: string,
-  details?: unknown
+  details?: unknown,
+  missingFields?: string[]
 ): ValidationError => {
   const error: ValidationError = new Error(message);
   error.name = 'ValidationError';
   if (details) {
     error.details = details;
+  }
+  if (missingFields) {
+    error.missingFields = missingFields;
   }
   return error;
 };
