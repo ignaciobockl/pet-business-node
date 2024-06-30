0cc824a5adb146d15f9641deea18ecd6bdb82253
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-06-30 12:46:10 -0300

    feat: validation of missing fields in the controller for user creation

--- src/controllers/user/userController.ts
+++ src/controllers/user/userController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 import { getStatusCode } from '../../middleware/errorHandler.ts';
 import * as userService from '../../services/userService.ts';
+import createValidationError from '../../utils/errors.ts';
 import logger from '../../utils/logger.ts';
 import handleResponse from '../../utils/responseHandler.ts';
 
@@ -33,6 +34,7 @@ export const getUsers = async (req: Request, res: Response): Promise<void> => {
   }
 };
 
+// eslint-disable-next-line complexity
 export const createUser = async (
   req: Request,
   res: Response
@@ -40,6 +42,18 @@ export const createUser = async (
   logger.info(`Incoming request: ${req.method} ${req.url}`);
   try {
     const { userName, password, mail, role } = req.body;
+
+    const missingFields = [];
+    if (!userName) missingFields.push('userName');
+    if (!password) missingFields.push('password');
+    if (!mail) missingFields.push('mail');
+    if (!role) missingFields.push('role');
+
+    if (missingFields.length > 0) {
+      const errorMessage = `Missing required fields: ${missingFields.join(', ')}`;
+      throw createValidationError(errorMessage, undefined, missingFields);
+    }
+
     const newUser = await userService.createUser({
       userName,
       password,
@@ -52,16 +66,24 @@ export const createUser = async (
       message: 'User created successfully',
       status: 201,
     });
-    logger.info(`Usuario ${userName} creado correctamente`);
+    logger.info(`User ${userName} successfully created`);
   } catch (error) {
     logger.error('Error creating user:', error);
     if (error instanceof Error) {
-      const statusCode = getStatusCode(error);
-      handleResponse(res, {
-        data: null,
-        message: error.message,
-        status: statusCode,
-      });
+      if (error.name === 'ValidationError') {
+        handleResponse(res, {
+          data: null,
+          message: error.message,
+          status: 400,
+        });
+      } else {
+        const statusCode = getStatusCode(error);
+        handleResponse(res, {
+          data: null,
+          message: error.message,
+          status: statusCode,
+        });
+      }
     } else {
       handleResponse(res, {
         data: null,
