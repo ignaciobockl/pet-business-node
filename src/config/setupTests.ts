d6da2e0fcf50f2f6c48a5177913fe0adf4b25f8a
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-07-06 13:14:02 -0300

    feat: environment configuration to run tests

--- src/config/setupTests.ts
+++ src/config/setupTests.ts
@@ -1,5 +1,9 @@
 import logger from '../utils/logger.ts';
 
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.test' });
+
 logger.info(
   `Runtime environment ${JSON.stringify(process.env.NODE_ENV)} in Port ${process.env.PORT}`
 );
