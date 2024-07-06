d54f4d9c29d722f75305155452a256a6bc13c5a9
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-07-06 12:51:06 -0300

    fix: node cannot be applied to manage environment variables due to incompatibility, for this reason dotenv will continue to be used for jest

--- src/config/setupTests.ts
+++ src/config/setupTests.ts
@@ -1,3 +1,5 @@
 import logger from '../utils/logger.ts';
 
-logger.info(`Runtime environment ${JSON.stringify(process.env.NODE_ENV)}`);
+logger.info(
+  `Runtime environment ${JSON.stringify(process.env.NODE_ENV)} in Port ${process.env.PORT}`
+);
