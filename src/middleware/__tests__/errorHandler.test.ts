2004e6e7dc8a41ae6557e37b31e96f7827fb400d
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-07-07 20:18:29 -0300

    test: handleResponse - should send a JSON response with the provided data, message, and status code

---
+++ src/middleware/__tests__/errorHandler.test.ts
@@ -0,0 +1,32 @@
+import { Response } from 'express';
+
+import { ResponseData } from '../../utils/interface/index.js';
+import handleResponse from '../../utils/responseHandler.ts';
+
+describe('handleResponse', () => {
+  let res: Partial<Response>;
+
+  beforeEach(() => {
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+  });
+
+  it('should send a JSON response with the provided data, message, and status code', () => {
+    const responseData: ResponseData = {
+      data: { id: 1, name: 'John Doe' },
+      message: 'Success',
+      status: 200,
+    };
+
+    handleResponse(res as Response, responseData);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      data: { id: 1, name: 'John Doe' },
+      message: 'Success',
+      status: 200,
+    });
+  });
+});
