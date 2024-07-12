8098b965466998295c916b54bcb740e10d30ce52
Ignacio Bockl <ignaciobockl96@gmail.com>
2024-07-12 11:44:57 -0300

    feat: route to get user by id

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import {
   createUserController,
   getAllUsersController,
+  getUserByIdController,
 } from '../controllers/user/userController.ts';
 
 const router = Router();
 
 router.get('/users', getAllUsersController);
+router.get('/user/:id', getUserByIdController);
 router.post('/user', createUserController);
 
 export default router;
